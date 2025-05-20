11486cee3f4116a05002c1ed2b59696d8932073b
WaledAliStudio <wpiixhub@gmail.com>
2025-05-20 10:51:25 +0300

    Delete build.sh

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# This is a simple build script for static site deployment
-echo "Starting build process..."
-# Copy all files to the output directory
-cp -r * $CLOUDFLARE_PAGES_OUTPUT_DIR/
-echo "Build completed successfully!" 
